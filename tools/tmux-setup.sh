6485ab3fa20cfffac6db8eee0054d678d42225eb
Daniel Jay Haskin <djhaskin987@gmail.com>
2026-05-31 10:56:24 -0600

    tmux

--- tools/tmux-setup.sh
+++ tools/tmux-setup.sh
@@ -5,4 +5,4 @@ tmux send-keys -l -t :.1 "./tools/clrepl"
 tmux send-keys -l -t :.1 "(asdf:load-system \"cl-mcp\")"
 tmux send-keys -l -t :.1 "(cl-mcp:start-http-server :port 12345)"
 (sleep 5 && tmux send-keys -l -t :.0 "./tools/goosew") &
-(sleep 20 && tmux send-keys -l -t :.0 "Go!") &
+(sleep 20 && tmux send-keys -l -t :.0 "Go!" && tmux send-keys -t :.01 -H "0d") &
