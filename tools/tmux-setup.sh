882a177461bab60aa848eeb14d44bba4577203cf
Daniel Jay Haskin <djhaskin987@gmail.com>
2026-05-31 10:37:47 -0600

    TMUX setup

--- tools/tmux-setup.sh
+++ tools/tmux-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-tmux split-window -h
+tmux split-window -v
 tmux send-keys -l -t :.1 "./tools/clrepl"
 tmux send-keys -l -t :.1 "(asdf:load-system \"cl-mcp\")"
 tmux send-keys -l -t :.1 "(cl-mcp:start-http-server :port 12345)"
-tmux send-keys -l -t :.1 "./tools/goosew"
+tmux send-keys -l -t :.0 "./tools/goosew"
+tmux send-keys -l -t :.0 "Please continue."
